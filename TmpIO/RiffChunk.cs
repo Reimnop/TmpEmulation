c3e148ffb995b631793436019918e64b9a2d09d1
Reimnop <rayngu2005@gmail.com>
2024-09-10 02:42:53 +0700

    Use SSBF instead

--- TmpIO/RiffChunk.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace TmpIO;
-
-public record RiffChunk(string Id, byte[] Data, IReadOnlyList<RiffChunk>? SubChunks = null);
--- TmpIO/TmpWriter.cs
+++ TmpIO/TmpWriter.cs
@@ -1,6 +1,5 @@
-using System.IO.Compression;
 using System.Runtime.InteropServices;
-using System.Text;
+using SimpleStructuredBinaryFormat;
 
 namespace TmpIO;
 
@@ -8,93 +7,66 @@ public static class TmpWriter
 {
     public static void Write(Stream stream, TmpFile file)
     {
-        var chunks = new List<RiffChunk>();
-        chunks.Add(GetMetadataChunk(stream, file.Metadata));
-        chunks.AddRange(file.Characters.Select(character => GetCharacterChunk(stream, character)));
-        chunks.AddRange(file.Glyphs.Select(glyph => GetGlyphChunk(stream, glyph)));
-        chunks.Add(GetAtlasChunk(stream, file.Atlas));
+        var obj = new SsbfObject
+        {
+            ["metadata"] = GetMetadata(file.Metadata),
+            ["characters"] = GetCharacters(file.Characters),
+            ["glyphs"] = GetGlyphs(file.Glyphs),
+            ["atlas"] = GetAtlas(file.Atlas),
+        };
         
-        WriteChunk(stream, new RiffChunk("RIFF", "TMPE"u8.ToArray(), chunks)); // RIFF header
-    }
-
-    private static RiffChunk GetMetadataChunk(Stream stream, TmpMetadata file)
-    {
-        using var memoryStream = new MemoryStream();
-        using var writer = new BinaryWriter(memoryStream, Encoding.UTF8, true);
-        writer.Write(file.Name.Length);
-        writer.Write(Encoding.UTF8.GetBytes(file.Name));
-        writer.Write(file.Size);
-        writer.Write(file.SdfRange);
-        
-        return new RiffChunk("META", memoryStream.ToArray());
+        SsbfWrite.WriteToStream(stream, obj, Compression.Gzip);
     }
     
-    private static RiffChunk GetCharacterChunk(Stream stream, in TmpCharacter character)
-    {
-        using var memoryStream = new MemoryStream();
-        using var writer = new BinaryWriter(memoryStream, Encoding.UTF8, true);
-        writer.Write(character.Character);
-        writer.Write(character.GlyphId);
-        
-        return new RiffChunk("CHAR", memoryStream.ToArray());
-    }
+    private static SsbfObject GetMetadata(TmpMetadata metadata)
+        => new()
+        {
+            ["name"] = metadata.Name,
+            ["size"] = metadata.Size,
+            ["sdfRange"] = metadata.SdfRange,
+        };
 
-    private static RiffChunk GetGlyphChunk(Stream stream, in TmpGlyph glyph)
+    private static SsbfArray GetCharacters(TmpCharacter[] characters)
     {
-        using var memoryStream = new MemoryStream();
-        using var writer = new BinaryWriter(memoryStream, Encoding.UTF8, true);
-        writer.Write(glyph.Id);
-        writer.Write(glyph.Advance);
-        writer.Write(glyph.BearingX);
-        writer.Write(glyph.BearingY);
-        writer.Write(glyph.Width);
-        writer.Write(glyph.Height);
-        writer.Write(glyph.MinX);
-        writer.Write(glyph.MinY);
-        writer.Write(glyph.MaxX);
-        writer.Write(glyph.MaxY);
-        
-        return new RiffChunk("GLPH", memoryStream.ToArray());
-    }
-
-    private static RiffChunk GetAtlasChunk(Stream stream, TmpAtlas atlas)
-    {
-        using var memoryStream = new MemoryStream();
-        using var writer = new BinaryWriter(memoryStream, Encoding.UTF8, true);
-        writer.Write(atlas.Width);
-        writer.Write(atlas.Height);
-        writer.Write(atlas.Data.Length * sizeof(float));
-
-        using var dataStream = new GZipStream(stream, CompressionMode.Compress, true);
-        writer.Write(MemoryMarshal.Cast<float, byte>(atlas.Data));
-        dataStream.Flush();
-        
-        return new RiffChunk("ATLS", memoryStream.ToArray());
+        var array = new SsbfArray(characters.Length);
+        foreach (var character in characters)
+        {
+            array.Add(new SsbfObject
+            {
+                ["character"] = character.Character,
+                ["glyphId"] = character.GlyphId,
+            });
+        }
+        return array;
     }
     
-    private static void WriteChunk(Stream stream, RiffChunk chunk)
+    private static SsbfArray GetGlyphs(TmpGlyph[] glyphs)
     {
-        using var writer = new BinaryWriter(stream, Encoding.UTF8, true);
-        
-        if (chunk.Id.Length != 4)
-            throw new ArgumentException("Chunk ID must be 4 characters long", nameof(chunk));
-        
-        // Round data length to multiple of 2
-        var dataLength = chunk.Data.Length;
-        if (dataLength % 2 != 0)
-            dataLength++;
-        
-        writer.Write(Encoding.ASCII.GetBytes(chunk.Id));
-        writer.Write(dataLength + (chunk.SubChunks?.Sum(x => x.Data.Length + 8) ?? 0));
-        writer.Write(chunk.Data);
-        
-        // Write padding
-        if (dataLength != chunk.Data.Length)
-            writer.Write((byte) 0);
-        
-        // Write sub-chunks
-        if (chunk.SubChunks is not null)
-            foreach (var subChunk in chunk.SubChunks)
-                WriteChunk(stream, subChunk);
+        var array = new SsbfArray(glyphs.Length);
+        foreach (var glyph in glyphs)
+        {
+            array.Add(new SsbfObject
+            {
+                ["id"] = glyph.Id,
+                ["advance"] = glyph.Advance,
+                ["bearingX"] = glyph.BearingX,
+                ["bearingY"] = glyph.BearingY,
+                ["width"] = glyph.Width,
+                ["height"] = glyph.Height,
+                ["minX"] = glyph.MinX,
+                ["minY"] = glyph.MinY,
+                ["maxX"] = glyph.MaxX,
+                ["maxY"] = glyph.MaxY,
+            });
+        }
+        return array;
     }
+    
+    private static SsbfObject GetAtlas(TmpAtlas atlas)
+        => new()
+        {
+            ["width"] = atlas.Width,
+            ["height"] = atlas.Height,
+            ["data"] = new SsbfByteArray(MemoryMarshal.Cast<float, byte>(atlas.Data)),
+        };
 }
